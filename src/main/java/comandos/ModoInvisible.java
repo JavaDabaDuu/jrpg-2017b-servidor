2d6f6437f087caae1867e36d5a0f93d212e3b629
leoavalos95 <leonelavalos.95@gmail.com>
2017-11-25 01:46:56 -0300

    El servidor no se enteraba quienes eran invisibles

---
+++ src/main/java/comandos/ModoInvisible.java
@@ -0,0 +1,14 @@
+package comandos;
+
+import mensajeria.PaquetePersonaje;
+import servidor.Servidor;
+
+public class ModoInvisible extends ComandosServer{
+	public void ejecutar() {
+				PaquetePersonaje paquetePersonaje = (PaquetePersonaje)getGson().fromJson(getCadenaLeida(), PaquetePersonaje.class);
+				Servidor.getPersonajesConectados().get(paquetePersonaje.getId()).setInvisibilidad();
+				synchronized (Servidor.getAtencionConexiones()) {
+					Servidor.getAtencionConexiones().notify();
+				}
+			}
+}
